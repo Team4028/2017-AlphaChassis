4e555823bb666f6459641e404766b65e23a02268
TheRealMrCool <adam.cool@chca-oh.org>
2017-02-19 21:34:55 -0500

    LIDAR check-in
    End of day check in for work with LIDAR

--- src/org/usfirst/frc/team4028/robot/sensors/Lidar.java
+++ src/org/usfirst/frc/team4028/robot/sensors/Lidar.java
@@ -1,36 +1,106 @@
 package org.usfirst.frc.team4028.robot.sensors;
 
-import org.usfirst.frc.team4028.robot.LogData;
+/*
+ * I was attempting to...
+ * 
+ * 1. Only send necessary distance values and make # the starting value and $ the ending value ("5#0034#0035#" becomes "5$#34$#35$
+ * 1b. find a way to get the position of #(starting value) and $(ending value) in the string
+ * 1c. then use those positions to find and extract the values between them
+ * 1d. then throw out all of the extracted characters except for ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+ * 
+ * 2. find a way to not record values when we do not have any... by either simply not recording or replace with the previous value
+ */
+import edu.wpi.first.wpilibj.SerialPort;
+import java.util.TimerTask;
+import java.lang.String;
 
-//This class implements all functionality for the Lidar Sensor
-//=====> For Changes see TBD
-public class Lidar 
-{
-	// constructor
-	public Lidar()
+public class LIDAR {
+	private java.util.Timer _updaterTimer;
+	private LIDARUpdaterTask _task;
+	private boolean _isRunning = false;
+	private  int _distance;
+	SerialPort _serialport;
+	
+	
+	public LIDAR(SerialPort.Port PortConstant)
 	{
-		
+		_task = new LIDARUpdaterTask();
+		_serialport = new SerialPort(9600, PortConstant);
+		_updaterTimer = new java.util.Timer();	
 	}
 	
-	//============================================================================================
-	// Methods follow
-	//============================================================================================	
+	public void start()
+	{
+		start(20);
+	}
+	
+	public void start(int period)
+	{
+		_isRunning = true;
+		_updaterTimer.scheduleAtFixedRate(_task, 0, period);
+	}
+	
+	public void stop()
+	{
+		_isRunning = false;
+		_updaterTimer.cancel();
+	}
+	
+	public void update()		// UPDATE DISTANCE VAR
+	{
+		try
+		{
+			String _RawDistanceText = _serialport.readString();
+			//int #pos = get(_RawDistanceText.charAt
+	
+	
+			//System.out.println(_serialport.readString());
+			//String _RawDistanceText = "#0035";
+			
+			//if(_RawDistanceText.startsWith('#'))
+			if(_RawDistanceText.charAt(1) == '#'){
+				_RawDistanceText = _RawDistanceText.substring(2);
+	//			   replace charAt(0)
+	//			_RawDistanceText.charAt(0);
+	//			 public static String removeCharAt(String s, int pos) {
+	//			      return s.substring(0, pos) + s.substring(pos + 1);
+	//			   }
+			}
+			_distance = Integer.parseInt(_RawDistanceText);
+		}
+		catch (NumberFormatException e)
+		{
+			e.printStackTrace();
+		}
+		catch (StringIndexOutOfBoundsException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public int get_Distance ()
+	{
+		return _distance;
+	}
 
-	public void Stop() 
-	{		
-	}
-	
-	// update the Dashboard with any Climber specific data values
-	public void OutputToSmartDashboard()
+	private class LIDARUpdaterTask extends TimerTask
 	{
+		public void run()
+		{
+			while (_isRunning)
+			{
+				update();
+				
+				try
+				{
+					Thread.sleep(10); //10 MS
+				}
+				catch (InterruptedException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
 	}
-	
-	public void UpdateLogData(LogData logData)
-	{
-	}
-	
-	//============================================================================================
-	// Property Accessors follow
-	//============================================================================================
-
 }
+
